7b5a7f9a061b50197291dde57b335eb2b3939554
talor-hammond <talorjhammond@gmail.com>
2018-10-16 17:52:14 +1300

    methods for issuing and creating jwts

---
+++ server/auth/token.js
@@ -0,0 +1,27 @@
+const jwt = require('jsonwebtoken');
+
+const { getUserByUsername } = require('../db/auth');
+
+// Method for issuing a jwt:
+function issue(req, res) {
+    const { username } = req.body
+
+    getUserByUsername(username)
+        .then(user => {
+            const token = createToken(user, process.env.JWT_SECRET);
+
+            res.json({
+                message: 'Authentication successful',
+                token
+            });
+        });
+};
+
+function createToken(user, secret) {
+    // const { id, username } = user
+    return jwt.sign( user, secret, { expiresIn: '1d'} );
+};
+
+module.exports = {
+    issue
+};
