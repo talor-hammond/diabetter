74d1e7e3c0932d4ef0010c81d09a7516e944056e
talor-hammond <talorjhammond@gmail.com>
2018-10-16 15:13:19 +1300

    register function for reg post route

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,7 +1,13 @@
 const router = require('express').Router();
 
-router.get('/', (req, res) => {
-    res.send('Hi');
-});
+// Routes:
+router.post('/register', register);
+
+// Helper-functions:
+function register (req, res) {
+    const { username, password } = req.body; // pulling the form-info out of the body of the req.
+    // TODO: Make sure username doesn't already exist.
+    // TODO: If not, hash the password and add the user to the database.
+}
 
 module.exports = router;
