8a1605343abb0d03fa335bc2e3cead97ef3ef8b0
talor-hammond <talorjhammond@gmail.com>
2018-10-16 15:30:25 +1300

    func that evaluates whether a userExists and returns a bool

--- server/db/auth.js
+++ server/db/auth.js
@@ -7,8 +7,15 @@ function createUser(user) {
 };
 
 // Checking whether a user exists; returning a bool:
-
+function userExists(username) {
+    return db('users')
+        .where('username', username)
+        .then(users => { // should return a max of 1; 0 if user doesn't exist.
+            return users.length > 0 // returns true if a user w provided username exists.
+        });
+};
 
 module.exports = {
-
+    createUser,
+    userExists
 };
