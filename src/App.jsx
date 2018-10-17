c5a6169dc56f4fdae3f83c404e9c09b05feb94e1
talor-hammond <talorjhammond@gmail.com>
2018-10-17 14:28:33 +1300

    initial routing setup w default components

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React, { Component, Fragment } from 'react';
+import { HashRouter as Router, Route } from 'react-router-dom';
+
+// Core-components:
+import { Header, Footer } from './components/layout';
+import { Home } from './components/home/Home';
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <Fragment>
+          <Route path="/" component={Header} />
+          <Route exact path="/" component={Home} />
+          <Route path="/" component={Footer} />
+        </Fragment>
+      </Router>
+    );
+  };
+};
+
+export default App;
---
+++ src/components/home/Home.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const Home = () => {
+    return (
+        <p>Here's where the home page will be</p>
+    );
+};
